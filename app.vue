eab78b6fb16365b32d1110b47c54dfe149e2f622
nata <104881649+natamz@users.noreply.github.com>
2022-09-28 08:34:32 +0900

    adjust style

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <v-app>
-    <Metronome></Metronome>
-    <tuner></tuner>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="6" class="pa-1">
+            <Metronome></Metronome>
+          </v-col>
+          <v-col cols="12" sm="6" class="pa-1">
+            <tuner></tuner>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
